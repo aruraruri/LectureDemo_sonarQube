536e5a5a21a5659931be89a340c6f98103c5e5c4
aruraruri <aurilait@gmail.com>
2025-11-20 13:31:49 +0200

    remove main

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -8,20 +8,6 @@ public class Calculator {
     private Calculator() {
 
     }
-
-    public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Calculator.class.getName());
-        Scanner sc = new Scanner(System.in);
-        logger.info("Enter first number");
-        double num1 = sc.nextDouble();
-
-        logger.info("Enter second number");
-        double num2 = sc.nextDouble();
-
-        double result = addValues(num1, num2);
-        String resultStr = "Result is "+result;
-        logger.info(resultStr);
-    }
     public static double addValues(double num1, double num2) {
         return num1 + num2;
     }
